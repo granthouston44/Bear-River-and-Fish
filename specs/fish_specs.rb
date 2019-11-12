559b18905e14c3756307681e694fac3f5a2960ad
Grant Houston <grant.houston44@hotmail.com>
2019-11-12 16:51:35 +0000

    done first fish test

---
+++ specs/fish_specs.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../fish.rb')
+
+class FishTest < MiniTest::Test
+
+def test_fish_name
+  fish = Fish.new("Trout")
+  assert_equal("Trout", fish.name)
+end
+
+
+
+
+
+end
